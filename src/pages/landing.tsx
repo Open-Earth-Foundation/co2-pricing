9ce9cee5c076ce03e35c6d5b9ed385cb03e7fadd
Felipe Mendonça <fysp11@gmail.com>
2022-10-31 19:59:43 +0100

    add Landing Page

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,37 +1,60 @@
 import * as React from 'react';
-import Grid from '@mui/material/Grid';
+import Grid from '@mui/material/Unstable_Grid2';
 
 import DescriptionBlock from '../components/ui/DescriptionBlock';
 
 import type { GetStaticProps } from 'next';
 import type { NextPageWithLayout } from "./_app";
-import { Typography } from '@mui/material';
-import Link from 'next/link';
+import { Link, Typography } from '@mui/material';
+import BaseLayout from '../layouts/BaseLayout';
+import LongButton from '../components/ui/LongButton';
 
 
 const Landing: NextPageWithLayout<DataProps> = ({ top, main }) => {
 
     return (
         <Grid container spacing={2}>
-            <Grid item xs={3} md={3}>
-                <h1>CO2 Pricing Oracle</h1>
+            <Grid md={3}>
+                <Typography variant="h3" component='h2'>CO2 Pricing Oracle</Typography>
             </Grid>
-            <Grid item xs={9} md={9}>
+            <Grid md={9}>
                 <DescriptionBlock description={top.description} title={top.title} />
             </Grid>
-            <Grid item xs={2} md={2}>
-                <h2>What is Carbon Pricing?</h2>
+            <Grid md={4}>
+                <Typography variant='h4' component="h3">How can I calculate mine?</Typography>
             </Grid>
-            <Grid item xs={10} md={10}>
+            <Grid md={8}>
                 <DescriptionBlock description={main.description} title={main.title}>
                     <Link href="/calculator">
                         <Typography>Learn More</Typography>
                     </Link>
                 </DescriptionBlock>
             </Grid>
-        </Grid>
+            <Grid md={4}>
+                <Typography variant='h4' component="h3">How can I calculate mine?</Typography>
+            </Grid>
+            <Grid md={8}>
+                <Link href="/calculator">
+                    <LongButton sx={{ background: '#262626' }}>Go to Calculator</LongButton>
+                </Link>
+            </Grid>
+            <Grid md={4}>
+                <Typography variant='h4' component="h3">
+                    I already have calculated mine
+                </Typography>
+            </Grid>
+            <Grid md={8}>
+                <Link href="/calculator">
+                    <LongButton variant="outlined">
+                        Check out how the RCB affected yours
+                    </LongButton>
+                </Link>
+            </Grid>
+        </Grid >
     );
 };
+
+Landing.getLayout = BaseLayout
 
 
 
@@ -57,7 +80,7 @@ export const getStaticProps: GetStaticProps<DataProps> = async () => {
             },
             main: {
                 title: "Main Description",
-                description: "Some description about the project and the goal involved in it",
+                description: "Some description about the project and the goal involved in it.".repeat(6),
                 more_info: "/more-info",
             }
         }
