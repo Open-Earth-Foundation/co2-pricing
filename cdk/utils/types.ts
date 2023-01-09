206d9bbbe108ad8f44c00f7362b8fe309974122c
Felipe Mendonça <fysp11@gmail.com>
2023-01-09 14:41:48 +0100

    chore: bump versions and minors

--- cdk/utils/types.ts
+++ cdk/utils/types.ts
@@ -1,20 +1,5 @@
-import * as glue from '@aws-cdk/aws-glue-alpha';
-
-
 export type PipelineConfig = {
     name: string,
     feature: 'fetch_csv' | 'local_file'
     source: string,
-}
-
-export interface TableConfig {
-    tableName: string;
-    columns: Record<string, glue.Type>
-    dataFormat: keyof glue.DataFormat;
-}
-
-export interface DatabaseConfig {
-    databaseName: string;
-    locationUri: string;
-    tables: TableConfig[];
 }
