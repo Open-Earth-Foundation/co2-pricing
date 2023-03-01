b5d3518c8093a80db0dd32119a3d11c45f9c6cf1
adit313 <app2116@gmail.com>
2023-03-01 17:27:54 -0500

    chore(Chart): Much Faster Speed

--- apps/web/src/pages/calculator/select.tsx
+++ apps/web/src/pages/calculator/select.tsx
@@ -5,79 +5,77 @@ import { useMemo, useState } from "react";
 import BaseLayout from "layouts/BaseLayout";
 import Chart from "@/components/ui/chart";
 
-import calculatorService from 'services/calculator';
-import iamService from 'services/iam';
+import calculatorService from "services/calculator";
+import iamService from "services/iam";
 
 import type { NextPageWithLayout } from "types/ui";
 import { SelectDiscountRate } from "ui-components";
 import { formatDiscount, formatPrice } from "utils/format";
 
-
-const CURRENT_YEAR = new Date().getFullYear()
+const CURRENT_YEAR = new Date().getFullYear();
 
 const SelectMethod: NextPageWithLayout = () => {
-    const [selectedModelId] = useState<string>('mimifund1')
-    const [discount, setDiscount] = useState(0.010)
-    const [canPlot, setCanPlot] = useState(true)
+  const [selectedModelId] = useState<string>("mimifund1");
+  const [discount, setDiscount] = useState(0.01);
+  const [canPlot, setCanPlot] = useState(true);
 
-    const reactivatePlot = () => setCanPlot(true)
+  const reactivatePlot = () => setCanPlot(true);
 
-    const iamModel = useQuery(
-        ['iam', selectedModelId], () => iamService.getModelById(selectedModelId), {
-        enabled: !!selectedModelId,
-    })
+  const iamModel = useQuery(
+    ["iam", selectedModelId],
+    () => iamService.getModelById(selectedModelId),
+    {
+      enabled: !!selectedModelId,
+    }
+  );
 
-    const dataPoints = useQuery(
-        ['mimifund', discount], () => calculatorService.getPlotData(discount), {
-        onSuccess: () => setCanPlot(false),
-        onError: () => setCanPlot(false),
-        initialData: [],
-        enabled: canPlot,
-    })
+  const dataPoints = calculatorService.getPlotData(discount);
 
-    const currentYearDataPoint = useMemo(() => {
-        return dataPoints.data?.find((dataPoint) => dataPoint.name === CURRENT_YEAR)
-    }, [dataPoints.data])
+  const currentYearDataPoint = useMemo(() => {
+    return dataPoints!.find((dataPoint) => dataPoint.name === CURRENT_YEAR);
+  }, [dataPoints]);
 
-    const [integer, decimal] = useMemo(() => {
-        if (!currentYearDataPoint) return ['0', '00']
-        return formatPrice(currentYearDataPoint.scc as number)
-    }, [currentYearDataPoint])
+  const [integer, decimal] = useMemo(() => {
+    if (!currentYearDataPoint) return ["0", "00"];
+    return formatPrice(currentYearDataPoint.scc as number);
+  }, [currentYearDataPoint]);
 
-    return (
-        <>
-            <SelectDiscountRate
-                width='100%'
-                integer={integer}
-                decimal={decimal}
-                discountPercent={formatDiscount(discount)}
-                slider={
-                    iamModel.data && <Slider
-                        aria-label="Discount rate"
-                        marks
-                        valueLabelDisplay="auto"
-                        onChange={(_, discount) => setDiscount(discount as number)}
-                        onChangeCommitted={reactivatePlot}
-                        value={discount}
-                        getAriaLabel={(index) => `${index} discount rate`}
-                        getAriaValueText={formatDiscount}
-                        valueLabelFormat={formatDiscount}
-                        {...iamModel.data.slider}
-                    />
-                }
-                chart={
-                    <Chart
-                        dataPoints={dataPoints.data}
-                        axisProp='name'
-                        dataProps={['scc']}
-                        xLabelProp='name'
-                    />
-                }
+  return (
+    <>
+      <SelectDiscountRate
+        width="100%"
+        integer={integer}
+        decimal={decimal}
+        discountPercent={formatDiscount(discount)}
+        slider={
+          iamModel.data && (
+            <Slider
+              aria-label="Discount rate"
+              marks
+              valueLabelDisplay="auto"
+              onChange={(_, discount) => setDiscount(discount as number)}
+              onChangeCommitted={reactivatePlot}
+              value={discount}
+              getAriaLabel={(index) => `${index} discount rate`}
+              getAriaValueText={formatDiscount}
+              valueLabelFormat={formatDiscount}
+              {...iamModel.data.slider}
             />
-        </>
-    )
+          )
+        }
+        chart={
+          <Chart
+            dataPoints={dataPoints!}
+            axisProp="name"
+            dataProps={["scc"]}
+            xLabelProp="name"
+          />
+        }
+      />
+    </>
+  );
 };
 
-SelectMethod.getLayout = BaseLayout
+SelectMethod.getLayout = BaseLayout;
 
 export default SelectMethod;
--- apps/web/src/types/calculator/plot.tsx
+++ apps/web/src/types/calculator/plot.tsx
@@ -1,1 +1,7 @@
-export type ChartDataPoint<T = Record<string, unknown>> = T extends Record<string, unknown> ? T & { [key: string]: string | number } : T
+export type ChartDataPoint<T = Record<string, unknown>> = T extends Record<
+  string,
+  unknown
+>
+  ? T & { [key: string]: string | number }
+  : T;
+export type ChartDataObject = { name: string; scc: number };
