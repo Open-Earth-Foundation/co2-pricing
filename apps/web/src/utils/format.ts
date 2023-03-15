57ba3ff47571ad3b95e914e42aece6f5e9ff5ca6
adit313 <app2116@gmail.com>
2023-03-14 22:32:56 -0400

    edit formatting

--- apps/web/src/utils/format.ts
+++ apps/web/src/utils/format.ts
@@ -1,10 +1,10 @@
 export const formatDiscount = (value: number) => {
-    return `${Number((value * 100).toFixed(3))}%`
-}
+  return `${Number((value * 100).toFixed(3))}%`;
+};
 
 export const formatPrice = (value: number): [string, string] => {
-    const price = Number(value.toFixed(2))
-    const integer = Math.floor(price)
-    const decimal = Math.round((price - integer) * 100)
-    return [integer.toFixed(0), decimal.toFixed(0)]
-}
+  const price = Number(value.toFixed(4));
+  const integer = Math.floor(price);
+  const decimal = Math.round((price - integer) * 100);
+  return [integer.toFixed(0), decimal.toFixed(0)];
+};
