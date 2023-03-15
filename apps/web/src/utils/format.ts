9da012772a36c3914b09d73f31420c795adc3b7e
adit313 <app2116@gmail.com>
2023-03-14 22:38:20 -0400

    fixed no leading 0 bug

--- apps/web/src/utils/format.ts
+++ apps/web/src/utils/format.ts
@@ -6,5 +6,9 @@ export const formatPrice = (value: number): [string, string] => {
   const price = Number(value.toFixed(4));
   const integer = Math.floor(price);
   const decimal = Math.round((price - integer) * 100);
+  let decimal_string = decimal.toFixed(0);
+  if (decimal_string.length === 1) {
+    decimal_string = "0" + decimal_string;
+  }
   return [integer.toFixed(0), decimal.toFixed(0)];
 };
