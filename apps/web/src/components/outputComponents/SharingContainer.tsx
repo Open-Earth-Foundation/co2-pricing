045fd4f9c981f7ed9522c96786339c7b6d4d6a0a
gaurab-pant <gaurab@openearth.org>
2023-04-18 19:11:16 -0400

    chore(styling changes): made some additional changes to sharing container

--- apps/web/src/components/outputComponents/SharingContainer.tsx
+++ apps/web/src/components/outputComponents/SharingContainer.tsx
@@ -8,15 +8,9 @@ import Image from 'next/image';
 import RemixIcon from '../../../public/remix_icon.svg';
 
 export default function SharingContainer() {
-  const goToSmartContract = useNavigateAction({
-    type: "url",
-    url: "https://remix.ethereum.org/Open-Earth-Foundation/co2-pricing/blob/main/apps/smartcontracts/demo.sol",
-  });
+  const goToSmartContract = () => window.open("https://remix.ethereum.org/Open-Earth-Foundation/co2-pricing/blob/main/apps/smartcontracts/demo.sol");
 
-  const goToAPI = useNavigateAction({
-    type: "url",
-    url: "https://app.swaggerhub.com/apis-docs/ADIT_2/CO2_pricing_oracle/1.0.0#/Carbon%20Pricing/co2Data",
-  });
+  const goToAPI = () => window.open("https://app.swaggerhub.com/apis-docs/ADIT_2/CO2_pricing_oracle/1.0.0#/Carbon%20Pricing/co2Data");
 
   const primary = "#2351dc";
 
@@ -29,45 +23,43 @@ export default function SharingContainer() {
   });
 
   return (
-    <div>
-      <span className="Start-sharing">
-        <ThemeProvider theme={theme}>
-          <Share className="Share-icon" fontSize="medium" color="primary" />
-        </ThemeProvider>
-        <span className="text-style-1">Start </span>
-        <span className="text-style-2">Sharing</span>
-      </span>
-      <div className="Sharing-container">
-        <div className="CTAs">
-          <div
-            className="share-button"
-            onClick={() => {
-              goToSmartContract();
-            }}
-          >
-            <Image
-              src={RemixIcon}
-              alt="Remix Icon"
-            />
-            <div className="share-text">
-              Open
-              <div className="second-text">in Remix</div>
-            </div>
+    <>
+      <div className="Start-sharing">
+        <div className="share-icon">
+          <Share className="share-icon" fontSize="inherit" color="primary" />
+        </div>
+        Start&nbsp;
+        <div className="second-text">sharing</div>
+      </div>
+      <div className="button-container">
+        <div
+          className="share-button"
+          onClick={() => {
+            goToSmartContract();
+          }}
+        >
+          <Image
+            src={RemixIcon}
+            alt="Remix Icon"
+          />
+          <div className="share-text">
+            Open
+            <div className="second-text">in Remix</div>
           </div>
-          <div
-            className="share-button"
-            onClick={() => {
-              goToAPI();
-            }}
-          >
-            <div className="share-icon"><AccountTreeOutlinedIcon fontSize="inherit" /></div>
-            <div className="share-text">
-              Connect
-              <div className="second-text">to our API</div>
-            </div>
+        </div>
+        <div
+          className="share-button"
+          onClick={() => {
+            goToAPI();
+          }}
+        >
+          <div className="connect-icon"><AccountTreeOutlinedIcon fontSize="inherit" /></div>
+          <div className="share-text">
+            Connect
+            <div className="second-text">to our API</div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
