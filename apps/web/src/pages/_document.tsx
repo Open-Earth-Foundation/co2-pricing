13cce8600e0d5de0167e22517333a19913268a5c
adit313 <app2116@gmail.com>
2023-04-26 12:32:49 -0400

    Testing Github Auto deploy

--- apps/web/src/pages/_document.tsx
+++ apps/web/src/pages/_document.tsx
@@ -1,16 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-
+import { Html, Head, Main, NextScript } from "next/document";
 
 const Document = () => {
-    return (
-        <Html>
-            <Head />
-            <body >
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    )
-}
+  return (
+    <Html>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
 
 export default Document;
+
+//testing
